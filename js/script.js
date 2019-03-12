f4ecc17b0c089d5f2dc4e6cb8842f00906a23ba6
Andrew Situ <gigles707@gmail.com>
2019-03-12 23:14:43 +0000

    search terms

--- js/script.js
+++ js/script.js
@@ -4,8 +4,18 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+  var searchTerm = $("#search-term").val();
+  var custom_url = `https://api.giphy.com/v1/gifs/search?q= ${searchTerm} &rating=pg&api_key=dc6zaTOxFJmzC`
+    $.ajax({
+        url: custom_url,
+        method: "GET",
+        success: function(response) {
+            // YOUR CODE GOES HERE
+            console.log(response);
+            $(".row.gallery").append("<img src='" + response.data[0].images.original.url + "'>");
+
+        }   
+    }); 
+    
 });
 
