630c2955acd3cd215ad56215be6648d4b570d523
Andrew Situ <gigles707@gmail.com>
2019-03-13 00:55:38 +0000

    show more images at once

--- js/script.js
+++ js/script.js
@@ -3,6 +3,26 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
+
+$("#random-button").click(function(){
+    var randomTerm = $("#random-term").val();
+    var custom_url = `https://api.giphy.com/v1/gifs/search?q= ${randomTerm} &rating=pg&api_key=dc6zaTOxFJmzC`
+    $.ajax({
+        url: custom_url,
+        method: "GET",
+        success: function(response) {
+            // YOUR CODE GOES HERE
+            console.log(response);
+            var gifs = Math.floor(Math.random() * response.data.length);
+            $(".text-center").replaceWith("<img class='text-center' src='" + response.data[gifs].images.original.url + "'>");
+        }
+    });
+});
+
+
+
+
+
 $("#search-button").click(function(){
   var searchTerm = $("#search-term").val();
   var custom_url = `https://api.giphy.com/v1/gifs/search?q= ${searchTerm} &rating=pg&api_key=dc6zaTOxFJmzC`
@@ -19,3 +39,19 @@ $("#search-button").click(function(){
     
 });
 
+$("#more-button").click(function(){
+  var searchTerm = $("#search-term").val();
+  var custom_url = `https://api.giphy.com/v1/gifs/search?q= ${searchTerm} &rating=pg&api_key=dc6zaTOxFJmzC`
+    $.ajax({
+        url: custom_url,
+        method: "GET",
+        success: function(response) {
+            // YOUR CODE GOES HERE
+            console.log(response);
+             for (var i=0; i<response.data.length; i++){
+          $(".row.gallery").append("<img src='" + response.data[i].images.original.url + "'>");
+            }
+        }   
+    }); 
+    
+});
